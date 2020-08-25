f687f247e3ee51b7e21203a499881d238ca9340a
Sergey Esipov <esipov.sergey.v@yandex.ru>
2020-08-25 17:40:11 +0300

    delete console.log

--- src/helpers/getColorsByIds.js
+++ src/helpers/getColorsByIds.js
@@ -1,6 +1,5 @@
 import colors from '@/data/colors';
 
 export default function getColorsByIds(colorIds) {
-  console.log('getColorsByIds');
   return colors.filter((color) => colorIds.includes(color.id));
 }
